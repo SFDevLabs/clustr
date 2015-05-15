5864695ff980c642490408e0c00b02109604e613
Eoin McMillan <eoinmcmillan@gmail.com>
2015-05-14 18:34:43 -0700

    home page mvp html done. form not hooked up

--- app/main/frontend/components/Header.react.js
+++ app/main/frontend/components/Header.react.js
@@ -13,7 +13,7 @@ var Header = React.createClass({
         <Link to="home"><img className="homeIcon" src="/img/logo_clustr_icon.png" title="home" /></Link>
       </div>
       <div className="statusBox">
-        <img className="statusBoxAddNode" src="img/addIcon_Color.png" title="add a node" />
+        <img className="statusBoxAddNode" src="img/circleAddButton.png" title="add a node" />
         <img className="statusBoxActivity" src="img/activity.png" title="check activity" />
         <img className="statusBoxProfile" title="profile" src="/img/eoin_profile.jpg" />
       </div>
