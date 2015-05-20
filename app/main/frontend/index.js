ed961ba15edc0cf1f7f96292ac00a016cf256fb5
jeffj <jenkinsjeffrey@gmail.com>
2015-05-20 14:28:30 -0700

    has update.

--- app/main/frontend/index.js
+++ app/main/frontend/index.js
@@ -13,8 +13,9 @@ var MainRoutes = require('./routes');
 //Other Routers
 var ArticleRoutes = require('../../articles/frontend/routes');
 
+debugger
 //remove all trailing slash
-window.history.pushState({}, '', window.location.pathname.replace(/(\/)+$/,""))
+window.history.pushState({}, '', window.location.pathname.replace(/(\/)+$/,"")+window.location.search+window.location.hash)
 
 //This is where we pass in our frontend routes files that structure the applicaiton.
 Router.run([ArticleRoutes, MainRoutes], Router.HistoryLocation, function(Root){
