7b2c31be7aead04a05a67ba7311e7385ffa0f674
jeffj <jenkinsjeffrey@gmail.com>
2015-05-20 17:14:26 -0700

    comment out UUID library for now.

--- app/articles/models/graph.js
+++ app/articles/models/graph.js
@@ -3,9 +3,11 @@
 // 
 var config = require('../../../config/config');
 var neo4j = require('neo4j');
+//var uuid = require('node-uuid');
 var db = new neo4j.GraphDatabase(
     config.graphdb
 );
+
 
 // private constructor:
 
