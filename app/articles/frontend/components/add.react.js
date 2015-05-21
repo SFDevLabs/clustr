46f9108976293acc2470a302b5b95c48063f25e7
Eoin McMillan <eoinmcmillan@gmail.com>
2015-05-20 23:21:04 -0700

    [stable WIP] add page

--- app/articles/frontend/components/add.react.js
+++ app/articles/frontend/components/add.react.js
@@ -23,7 +23,72 @@ var ArticleActions = require('../actions/ArticleActions');
 var MainSearch = React.createClass({
   render: function() {
     return (
-      <div>placeHolder Add</div>
+      <div className="addPageContainer">
+        <ul className="row sixteen marginZero connection">
+          <li className="columns three"><img src="img/blank.png" /></li>
+          <li className="columns four">
+            <div className="leftBox">
+              <ul className="row sixteen marginZero">
+                <li className="columns four addIconBox"><img className="addIcon" src="img/twitter_bird.png" /></li>
+                <li className="columns ten nodeTitleBox">
+                  <div className="nodeTitle">Twitter.com
+                  </div>
+                  <div className="nodeUrl">http://Twitter.com</div>
+                </li>
+                <li className="columns two"><img src="img/blank.png" /></li>
+              </ul>
+            </div>
+          </li>
+          <li className="columns two"><img className="connectMetaphor" src="img/connect_metaphor.png" /></li>
+          <li className="columns four">
+            <div className="rightBox">
+              <ul className="row sixteen marginZero">
+                <li className="columns four addIconBox"><img className="addIcon" src="img/foursquare.png" /></li>
+                <li className="columns ten nodeTitleBox">
+                  <div className="nodeTitle">Foursquare.com
+                  </div>
+                  <div className="nodeUrl">http://foursquare.com</div>
+                </li>
+                <li className="columns two"><img src="img/blank.png" /></li>
+              </ul>
+            </div>
+          </li>
+          <li className="columns three"><img src="img/blank.png" /></li>
+        </ul>
+
+        <ul className="row sixteen marginZero connection">
+          <li className="columns three"><img src="img/blank.png" /></li>
+          <li className="columns four">
+            <div className="leftBox">
+              <ul className="row sixteen marginZero">
+                <li className="columns four addIconBox"><img className="addIcon" src="img/pinkPanther.jpg" /></li>
+                <li className="columns ten nodeTitleBox">
+                  <div className="nodeTitle">The Pink Panther
+                  </div>
+                  <div className="nodeUrl">http://www.amazon.com/Pink-Panther-Shawn-Levy/dp/B001ML9LSS/ref=sr_1_1?ie=UTF8&qid=1432189177&sr=8-1&keywords=pink+panther</div>
+                </li>
+                <li className="columns two"><img src="img/blank.png" /></li>
+              </ul>
+            </div>
+          </li>
+          <li className="columns two"><img className="connectMetaphor" src="img/connect_metaphor.png" /></li>
+          <li className="columns four">
+            <div className="rightBox">
+              <ul className="row sixteen marginZero">
+                <li className="columns four addIconBox"><img className="addIcon" src="img/OSS_117.jpg" /></li>
+                <li className="columns ten nodeTitleBox">
+                  <div className="nodeTitle">OSS 117
+                  </div>
+                  <div className="nodeUrl">http://dvd.netflix.com/Movie/OSS-117-Cairo-Nest-of-Spies/70101687</div>
+                </li>
+                <li className="columns two"><img src="img/blank.png" /></li>
+              </ul>
+            </div>
+          </li>
+          <li className="columns three"><img src="img/blank.png" /></li>
+        </ul>
+
+      </div>
       );
     }
 });
