f8353f9b7e0a06ef61e7d7bf999f72b800125c4b
jeffj <jenkinsjeffrey@gmail.com>
2015-05-20 23:25:03 -0700

    updating app and error message.

--- app/articles/frontend/components/add.react.js
+++ app/articles/frontend/components/add.react.js
@@ -16,16 +16,66 @@ var React = require('react');
 var ArticleStore = require('../stores/ArticleStore');
 var ItemInput = require('./ItemInput.react');
 var ArticleActions = require('../actions/ArticleActions');
-
-
+var ReactPropTypes = React.PropTypes;
 
 
 var MainSearch = React.createClass({
-  render: function() {
-    return (
-      <div>placeHolder Add</div>
-      );
-    }
+  
+	propTypes: {
+		value: ReactPropTypes.string
+	},
+
+	getInitialState: function() {
+		return {
+		  value: this.props.value || ''
+		};
+	},
+
+
+	render: function() {
+	return (
+	  <div>
+	    <input
+	      type="text" 
+	      placeholder="Input a URL"
+	      onChange={this._onChange}
+	      onKeyDown={this._onKeyDown}
+	      value={this.state.value}
+	      autoFocus={true}
+	    />
+	  </div>
+	  );
+	},
+
+	/**
+	* Invokes the callback passed in as onSave, allowing this component to be
+	* used in different ways.
+	*/
+	_save: function() {
+		if (this.state.value && this.state.value.length>0){
+		  this.transitionTo('articles',{},{q: this.state.value});
+		};
+	},
+  
+
+	/**
+	* @param {object} event
+	*/
+	_onChange: function(/*object*/ event) {
+	this.setState({
+	  value: event.target.value
+	});
+	},
+
+	/**
+	* @param  {object} event
+	*/
+	_onKeyDown: function(event) {
+	if (event.keyCode === ENTER_KEY_CODE) {
+	  this._save();
+	}
+	}
+
 });
 
 module.exports = MainSearch;
