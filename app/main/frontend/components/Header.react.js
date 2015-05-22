07569c26251a8923f00140832cf0941bea7848bb
Eoin McMillan <eoinmcmillan@gmail.com>
2015-05-22 02:38:23 -0700

    search bar added to header

--- app/main/frontend/components/Header.react.js
+++ app/main/frontend/components/Header.react.js
@@ -7,15 +7,26 @@ var React = require('react');
 var Link = require('react-router').Link;
 
 var userNameDiv = document.getElementById("user-name");
-var userName = userNameDiv.dataset.name
+var userName = userNameDiv.dataset.name;
 
+var URLSearchInput = require('../../../articles/frontend/components/URLSearchInput.react');
 
 var Header = React.createClass({
+
   render: function () {
     return <header>
       <div className="homeBox">
         <Link to="articles"><img className="homeIcon" src="/img/logo_clustr_icon.png" title="home" /></Link>
         <span>{userName}</span>
+      </div>
+     <div className = "headerSearch">
+         <URLSearchInput
+          id=""
+          placeholder="URL Search..."
+          onSave={this._onSave}
+          className="queryBox"
+          name = "query"
+        />
       </div>
       <div className="statusBox">
         <Link to="add"><img className="statusBoxAddNode" src="img/circleAddButton.png" title="add a node" /></Link>
